3e953d280f13c279216c9dd88fd9ede944a7e6ff
apanyukam <apanyuk@gmail.com>
2022-12-10 16:27:03 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+
+        // Решение как в задании
+
+        /*
+        System.out.println("1 * 0 = 0");
+        System.out.println("1 * 1 = 1");
+        System.out.println("1 * 2 = 2");
+        System.out.println("1 * 3 = 3");
+        System.out.println("1 * 4 = 4");
+        System.out.println("1 * 5 = 5");
+        System.out.println("1 * 6 = 6");
+        System.out.println("1 * 7 = 7");
+        System.out.println("1 * 8 = 8");
+        System.out.println("1 * 9 = 9");
+        */
+
+        // Решение с циклом
+        for (int i = 0; i < 10; i++) {
+            var j = i * 1;
+            System.out.println("1" + " * " + i + " = " + j);
+        }
+    }
+}
